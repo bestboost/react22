741188f363c4d4bf6dc4b34572a3f1706de17330
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-07-22 11:47:22 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-// import { Switch, Route } from 'react-router-dom';
-// import AppBar from './components/AppBar/AppBar';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
 import SignupForm from './components/SignupForm/SignupForm';
-// import ColorPicker from './components/ColorPicker/ColorPicker';
-// import Counter from './components/Counter/Counter';
-// import Clock from './components/Clock/Clock';
+import ColorPicker from './components/ColorPicker/ColorPicker';
+import Counter from './components/Counter/Counter';
+import Clock from './components/Clock/Clock';
 // import PokemonView from './views/PokemonView';
+import Navigation from './components/Navigation/Navigation'
 
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 const containerStyles = {
   maxWidth: 1170,
@@ -26,29 +27,30 @@ const containerStyles = {
 export default function App() {
   return (
     <div style={containerStyles}>
-      {/* <AppBar /> */}
+      <AppBar />
+      <Navigation/>
 
-      {/* <Switch> */}
-        {/* <Route path="/signup">
+      <Switch> 
+         <Route path="/signup">
           <SignupForm />
-        </Route> */}
- <SignupForm />
-        {/* <Route path="/colorpicker"> */}
-          {/* <ColorPicker options={colorPickerOptions} /> */}
-        {/* </Route> */}
+        </Route>
 
-        {/* <Route path="/counter"> */}
-          {/* <Counter /> */}
-        {/* </Route> */}
+        <Route path="/colorpicker"> 
+          <ColorPicker options={colorPickerOptions} />
+         </Route>
 
-        {/* <Route path="/clock"> */}
-          {/* <Clock /> */}
-        {/* </Route> */}
+        <Route path="/counter"> 
+           <Counter /> 
+         </Route>
+
+        <Route path="/clock"> 
+           <Clock /> 
+        </Route>
 
         {/* <Route path="/pokemon">
           <PokemonView />
         </Route> */}
-      {/* </Switch> */}
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // import AuthProvider from './contexts/auth/Provider';
 import 'modern-normalize/modern-normalize.css';
@@ -9,10 +9,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
       {/* <AuthProvider> */}
         <App />
       {/* </AuthProvider> */}
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
