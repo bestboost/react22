a9d2185f66661e1cfc91d4451f253501e8f793c5
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-09-03 21:47:22 +0300

    +

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,13 +3,7 @@ import { Link } from 'react-router-dom';
 export const Layout = () => {
   return (
     <div>
-      <nav>
-        <Link to="/player">Player</Link>
-        <br />
-        <Link to="/reader">Reader</Link>
-        <br />
-        <Link to="images">Images</Link>
-      </nav>
+      <nav></nav>
     </div>
   );
 };
