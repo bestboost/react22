c608e4958a1baa4a2da43153b8ef9c503be9b52a
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-09-05 12:00:11 +0300

    +

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,17 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/store';
 
 export const Layout = () => {
+  const dispatch = useDispatch();
+  const value = useSelector(state => state.myValue);
+
   return (
     <div>
-      <nav></nav>
+      {value}
+      <br />
+      <button onClick={() => dispatch(increment(100))}>Increment</button>
+      <button onClick={() => dispatch(decrement(100))}>Decrement</button>
     </div>
   );
 };
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,14 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const myReducer = createReducer(10, {});
-
-export const store = configureStore({
-  reducer: {
-    myValue: myReducer,
+const myValueSlice = createSlice({
+  name: 'myValue',
+  initialState: 100,
+  reducers: {
+    increment(state, action) {
+      return state + action.payload;
+    },
+    decrement(state, action) {
+      return state - action.payload;
+    },
   },
 });
 
-const enhancer = devToolsEnhancer();
-console.log('enhancer:', enhancer);
+export const { increment, decrement } = myValueSlice.actions;
+
+const itemsSlice = createSlice({
+  name: 'items',
+  initialState: [],
+  redusers: {
+    add(state, action) {
+      state.push(action.payload);
+    },
+    remove(state, action) {
+      return state.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { add, remove } = itemsSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    myValue: myValueSlice.reducer,
+    items: itemsSlice.reducer,
+  },
+});
